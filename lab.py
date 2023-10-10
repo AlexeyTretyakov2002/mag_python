571ffea7f51134cda8c1b61a86e8c9159b6c8381
Alexey Tretyakov <alex@tretyakov2002.ru>
2023-10-10 09:33:32 +0300

    Получение и вывод яндекса

--- lab.py
+++ lab.py
@@ -1,1 +1,7 @@
-testtest
+import os
+import requests
+
+URL = "https://yandex.ru/"
+html_page = requests.get(URL, headers={"User-Agent":"Mozilla/5.0"})
+print(html_page.text) 
+# html_page.text - хранит html код веб-страницы
