5924c11394b7e7d0fe1049fdc94b6d06e5f0317a
Alexey Tretyakov <alex@tretyakov2002.ru>
2023-10-10 10:49:59 +0300

    test3

--- lab.py
+++ lab.py
@@ -1,7 +1,36 @@
-import os
-import requests
+# Первый вариант
+# import requests
+# from bs4 import BeautifulSoup
+# URL = "https://www.kinopoisk.ru/film/435/"
+# headers = {"User-Agent": "Mozilla/5.0"}
+# response = requests.get(URL, headers=headers)
+# html_page = response.text
+# soup = BeautifulSoup(html_page, "html.parser")
+# print(soup.prettify())
 
-URL = "https://yandex.ru/"
-html_page = requests.get(URL, headers={"User-Agent":"Mozilla/5.0"})
-print(html_page.text) 
-# html_page.text - хранит html код веб-страницы
+
+# Второй вариант для получения полного кода
+# from selenium import webdriver
+# URL = "https://www.kinopoisk.ru/film/435/"
+# # Инициализация драйвера браузера
+# driver = webdriver.Chrome()
+# # Загрузка страницы
+# driver.get(URL)
+# # Получение полного HTML-кода страницы
+# html_code = driver.page_source
+# # Закрытие браузера
+# driver.quit()
+# print(html_code)
+
+
+from selenium import webdriver
+# Указываем путь к драйверу Chrome
+driver = webdriver.Chrome( executable_path="D://Study/Magistracy/Semester_1/НовыеТехнологии(Корнеева)/chromedriver.exe")
+# Загружаем страницу
+URL = "https://www.kinopoisk.ru/film/435/"
+driver.get(URL)
+# Получаем полный текст страницы
+html_page = driver.page_source
+# Закрываем браузер
+driver.quit()
+print(html_page)
