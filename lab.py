7dc4a9c757e0b0cc6284120a2e55f447e2b42cb9
Alexey Tretyakov <alex@tretyakov2002.ru>
2023-10-10 12:56:13 +0300

    Пробую циклом записать несколько страниц рецензий

--- lab.py
+++ lab.py
@@ -1,34 +1,35 @@
-# Первый вариант
-# import requests
-# from bs4 import BeautifulSoup
-# URL = "https://www.kinopoisk.ru/film/435/"
+#Первый вариант
+import os
+import requests
+import time
+from bs4 import BeautifulSoup
+# Чтение одной страницы
+# URL = "https://www.kinopoisk.ru/film/535341/reviews/ord/rating/status/bad/perpage/10/page/1/"
 # headers = {"User-Agent": "Mozilla/5.0"}
 # response = requests.get(URL, headers=headers)
 # html_page = response.text
 # soup = BeautifulSoup(html_page, "html.parser")
-# print(soup.prettify())
+# htmlText = soup.prettify()
 
-
-# Второй вариант для получения полного кода
-import os
-from selenium import webdriver
-from selenium.webdriver import Firefox
-from selenium.webdriver.common.keys import Keys
-# Загружаем страницу
-URL = "https://www.kinopoisk.ru/film/535341/reviews/ord/rating/status/bad/perpage/10/page/1/"
-driver = Firefox()
-driver.get(URL)
-# Получаем полный текст страницы
-html_page = driver.page_source
-
-# Закрываем браузер
-driver.quit()
+textResult = ""
+num_pos_reviews  = 0
+num_neg_reviews  = 0
+downloaded_count = 0
+# Цикл по страницам рецензий (5, чтобы не заблочило капчей)
+for page in range(1, 10):
+    search_url = f'https://www.kinopoisk.ru/film/535341/reviews/ord/rating/status/bad/perpage/10/page/{page}/'
+    response = requests.get(search_url, headers={"User-Agent":"Mozilla/5.0"})
+    response.encoding = 'utf-8' 
+    soup = BeautifulSoup(response.text, "html.parser")
+    if(response.status_code == 200):
+        textResult += soup.prettify() 
+    else:
+        print("Некорректная загрузка страницы" + page)
+    time.sleep(5)
 
 # Записываем текст элемента в файл
 # Получаем путь к текущей директории
 current_dir = os.path.dirname(os.path.abspath(__file__))
 file_path = os.path.join(current_dir, "example.txt")
 with open(file_path, "w") as file:
-    file.write(html_page)
-
-print(html_page)
+    file.write(textResult)
