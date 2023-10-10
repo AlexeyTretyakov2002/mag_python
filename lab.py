98aea42d46cfb4fc0b48452c21c9c5c106f2db02
Alexey Tretyakov <alex@tretyakov2002.ru>
2023-10-10 11:24:54 +0300

    Получилось прочитать одну вкладку с рецензиями

--- lab.py
+++ lab.py
@@ -10,27 +10,25 @@
 
 
 # Второй вариант для получения полного кода
-# from selenium import webdriver
-# URL = "https://www.kinopoisk.ru/film/435/"
-# # Инициализация драйвера браузера
-# driver = webdriver.Chrome()
-# # Загрузка страницы
-# driver.get(URL)
-# # Получение полного HTML-кода страницы
-# html_code = driver.page_source
-# # Закрытие браузера
-# driver.quit()
-# print(html_code)
-
-
+import os
 from selenium import webdriver
-# Указываем путь к драйверу Chrome
-driver = webdriver.Chrome( executable_path="D://Study/Magistracy/Semester_1/НовыеТехнологии(Корнеева)/chromedriver.exe")
+from selenium.webdriver import Firefox
+from selenium.webdriver.common.keys import Keys
 # Загружаем страницу
-URL = "https://www.kinopoisk.ru/film/435/"
+URL = "https://www.kinopoisk.ru/film/535341/reviews/ord/rating/status/bad/perpage/10/page/1/"
+driver = Firefox()
 driver.get(URL)
 # Получаем полный текст страницы
 html_page = driver.page_source
+
 # Закрываем браузер
 driver.quit()
+
+# Записываем текст элемента в файл
+# Получаем путь к текущей директории
+current_dir = os.path.dirname(os.path.abspath(__file__))
+file_path = os.path.join(current_dir, "example.txt")
+with open(file_path, "w") as file:
+    file.write(html_page)
+
 print(html_page)
