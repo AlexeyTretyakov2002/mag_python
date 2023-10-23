d15a724f495f9bfb50a8b628492690251febe715
Alexey Tretyakov <alex@tretyakov2002.ru>
2023-10-23 23:21:20 +0300

    Создание csv

--- lab2.py
+++ lab2.py
@@ -1,1 +1,34 @@
+import os
+import pandas as pd
+import dateparser
 
+data = []
+
+def process_review_file(file_path, rating):
+    with open(file_path, "r", encoding="utf-8") as file:
+        lines = file.readlines()
+        title = lines[0].strip()
+        header = lines[1].strip()
+        text = ""
+        for line in lines[2:-1]:
+            text += line.strip() + " "
+        date_str = lines[-1].strip()
+        date = dateparser.parse(date_str)
+        iso_date = date.strftime("%Y-%m-%dT%H:%M:%S")   
+        data.append([title, rating, header, text, iso_date])
+
+# Обработка файлов с хорошими рецензиями
+good_reviews_dir = "./dataset/good"
+for file_name in os.listdir(good_reviews_dir):
+    file_path = os.path.join(good_reviews_dir, file_name)
+    process_review_file(file_path, "+")
+
+# Обработка файлов с плохими рецензиями
+bad_reviews_dir = "./dataset/bad"
+for file_name in os.listdir(bad_reviews_dir):
+    file_path = os.path.join(bad_reviews_dir, file_name)
+    process_review_file(file_path, "-")
+
+# Создание DataFrame
+df = pd.DataFrame(data, columns=["Title", "Rating", "Header", "Text", "Date"])
+df.to_csv("reviews_dataset.csv", index=False, encoding="utf-8", sep="|")
